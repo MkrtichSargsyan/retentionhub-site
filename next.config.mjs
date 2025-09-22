d22268b4eb12f08f0489935a41dacb83fa73743c
mkrtich <mkrtichsargsyan24@gmail.com>
2025-09-22 07:56:29 +0400

    Fixed config file

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     output: "export",       // enable static export
     basePath: "/retentionhub-site", // your GitHub repo name
     trailingSlash: true,
+    assetPrefix: '/retentionhub-site/'
 };
 
 export default nextConfig;
